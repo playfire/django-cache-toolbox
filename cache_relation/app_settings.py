65b4c081c3a66ccd373062f8e7c1d63295c8d8d1
Chris Lamb <chris@playfire.com>
2011-06-23 14:26:46 +0100

    Allow global settings to override.
    Signed-off-by: Chris Lamb <chris@playfire.com>

--- cache_relation/app_settings.py
+++ cache_relation/app_settings.py
@@ -1,2 +1,8 @@
-# Default cache duration
-CACHE_RELATION_DEFAULT_DURATION = 60 * 60 * 24 * 3
+from django.conf import settings
+
+# Default cache timeout
+CACHE_RELATION_DEFAULT_DURATION = getattr(
+    settings,
+    'CACHE_RELATION_DEFAULT_DURATION',
+    60 * 60 * 24 * 3,
+)
