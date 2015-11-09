a601b2ec4f4f6796dc5fa8ed613d1b834001a367
Alistair Lynn <arplynn@gmail.com>
2015-11-09 15:16:21 +0000

    Fix the authentication middleware
    At some point `request.session[SESSION_KEY]` started returning a `unicode`
    rather than an `int`. As a result this would always fail, and thus always
    fall back to the parent mechanism.

--- cache_toolbox/middleware.py
+++ cache_toolbox/middleware.py
@@ -91,7 +91,7 @@ class CacheBackedAuthenticationMiddleware(AuthenticationMiddleware):
     def process_request(self, request):
         try:
             # Try and construct a User instance from data stored in the cache
-            request.user = User.get_cached(request.session[SESSION_KEY])
+            request.user = User.get_cached(int(request.session[SESSION_KEY]))
         except:
             # Fallback to constructing the User from the database.
             super(CacheBackedAuthenticationMiddleware, self).process_request(request)
