e56f1076d9e62c4adb57bf99d20bc55e382f2c48
Chris Lamb <chris@chris-lamb.co.uk>
2016-03-14 14:57:03 +0000

    Releasing version 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(
     name='django-cache-toolbox',
     description="Non-magical object caching for Django.",
-    version='0.1',
+    version='0.1.1',
     url='http://code.playfire.com/django-cache-toolbox',
 
     author='Playfire.com',
