e701e42a1aa8008f8bbc5c0a7f30729e82b00658
Chris Lamb <chris@chris-lamb.co.uk>
2018-07-20 22:50:42 +0800

    Releasing version 1.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(
     name='django-cache-toolbox',
     description="Non-magical object caching for Django.",
-    version='0.3.2',
+    version='1.0.0',
     url='https://chris-lamb.co.uk/projects/django-cache-toolbox',
 
     author='Chris Lamb',
