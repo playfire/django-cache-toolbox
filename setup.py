59cfc2e367bf0c293cb066b36fe709247f5c067e
Peter Law <PeterJCLaw@gmail.com>
2020-03-13 18:47:13 +0000

    Include the README as the package long description

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
 
+import os.path
 from setuptools import setup, find_packages
+
+my_dir = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(my_dir, 'README.md')) as f:
+    long_description = f.read()
 
 setup(
     name='django-cache-toolbox',
     description="Non-magical object caching for Django.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     version='1.1.1',
     url='https://chris-lamb.co.uk/projects/django-cache-toolbox',
 
